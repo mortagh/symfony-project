f2149163494385b26b94144ed822ab4f254b0953
Mamadou <mamad33sylla@gmail.com>
2022-01-08 15:53:55 +0100

    easy admin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Album;
+use App\Entity\Article;
+use App\Entity\Artiste;
+use App\Entity\Musique;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function index(): Response
+    {
+        return $this->render('admin/dashboard.html.twig');
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            ->setTitle('Symfony');
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Album', 'fas fa-list', Album::class);
+        yield MenuItem::linkToCrud('Article', 'fas fa-list', Article::class);
+        yield MenuItem::linkToCrud('Artiste', 'fas fa-list', Artiste::class);
+        yield MenuItem::linkToCrud('Musique', 'fas fa-list', Musique::class);
+
+    }
+}
