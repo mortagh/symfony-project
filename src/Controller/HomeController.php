e9da77e42d75cb32730426cf9a4924403a2b2ed8
carlosvieira <carlosvieira278@gmail.com>
2022-01-10 15:11:07 +0100

    changement

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -7,12 +7,15 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 
- /**
-     * @Route("/")
-     */
 class HomeController extends AbstractController
 {
-   
+    /**
+     * @Route("/")
+     */
+    public function index(): Response
+    {
+        return $this->redirectToRoute('home');
+    }
     /**
      * @Route("/home", name="home")
      */
