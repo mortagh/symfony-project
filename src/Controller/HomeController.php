04f483e23fde583de08f78117dfe1135b1abfc66
Mamadou <mamad33sylla@gmail.com>
2022-01-12 11:46:06 +0100

    home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,7 +1,11 @@
 <?php
 
 namespace App\Controller;
-
+use App\Entity\Album;
+use App\Entity\Artiste;
+use App\Repository\AlbumRepository;
+use App\Repository\ArtisteRepository;
+use App\Repository\MusiqueRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,12 +23,19 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function home(Request $request): Response
+    public function home(Request $request,AlbumRepository $albumRepository,ArtisteRepository $artisteRepository,MusiqueRepository $musiqueRepository): Response
     {
+        $albums = $albumRepository->findBy([],['id'=>'DESC']);
+        $artistes = $artisteRepository->findBy([],['id'=>'DESC']);
+        $musiques = $musiqueRepository->findBy([],['id'=>'DESC']);
         $currentPage = $request->getPathInfo();
         return $this->render('home/index.html.twig', [
             'currentPage' => $currentPage,
+            'albums' => $albums,
+            'artistes' => $artistes,
+            'musiques' => $musiques,
         ]);
     }
 }
+
 
