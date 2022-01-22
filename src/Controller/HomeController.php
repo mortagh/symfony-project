bf6eac4daf20ae407f8b410217947558cf7a7474
carlosvieira <carlosvieira278@gmail.com>
2022-01-22 13:27:46 +0100

    gergreg

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,6 +6,7 @@ use App\Entity\Artiste;
 use App\Repository\AlbumRepository;
 use App\Repository\ArtisteRepository;
 use App\Repository\MusiqueRepository;
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -23,17 +24,19 @@ class HomeController extends AbstractController
     /**
      * @Route("/home", name="home")
      */
-    public function home(Request $request,AlbumRepository $albumRepository,ArtisteRepository $artisteRepository,MusiqueRepository $musiqueRepository): Response
+    public function home(Request $request,AlbumRepository $albumRepository,ArtisteRepository $artisteRepository,MusiqueRepository $musiqueRepository,ArticleRepository $articleRepository): Response
     {
         $albums = $albumRepository->findBy([],['id'=>'DESC']);
         $artistes = $artisteRepository->findBy([],['id'=>'DESC']);
         $musiques = $musiqueRepository->findBy([],['id'=>'DESC']);
         $currentPage = $request->getPathInfo();
+        $articles = $articleRepository->findBy([],['id'=>'DESC']);
         return $this->render('home/index.html.twig', [
             'currentPage' => $currentPage,
             'albums' => $albums,
             'artistes' => $artistes,
             'musiques' => $musiques,
+            'articles' => $articles,
         ]);
     }
 }
